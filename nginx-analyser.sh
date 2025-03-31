3a21bf98ea89c2d325f1f358e3d9953f3a70d9eb
lethanix <developer@lethanix.com>
2025-03-31 00:58:14 -0600

    Add feature: top 5 most requested paths

--- nginx-analyser.sh
+++ nginx-analyser.sh
@@ -6,9 +6,15 @@ LOG=nginx-access.log
 
 ######################################################
 echo Top 5 IP addresses with the most requests:
-awk '{print $1}' $LOG | sort \
-	| uniq -c | sort -nr | head -5 \
+awk '{print $1}' $LOG | sort | uniq -c | sort -nr | head -5 \
 	| awk '{printf "%s - %d requests\n", $2, $1}'
 
 ######################################################
+echo
+echo Top 5 most requested paths:
+grep -o '\w\s/.*\sHTTP' $LOG | awk '{print $2}' | sort | uniq -c | sort -nr | head -5 \
+	| awk '{printf "%s - %d requests\n", $2, $1}'
+
+
+
 
