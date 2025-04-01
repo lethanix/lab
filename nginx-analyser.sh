fae3187211ca2b2d65eb588947fa95e4e266bd54
lethanix <developer@lethanix.com>
2025-03-31 22:37:46 -0600

    Add feature: top 5 status codes

--- nginx-analyser.sh
+++ nginx-analyser.sh
@@ -15,6 +15,12 @@ echo Top 5 most requested paths:
 grep -o '\w\s/.*\sHTTP' $LOG | awk '{print $2}' | sort | uniq -c | sort -nr | head -5 \
 	| awk '{printf "%s - %d requests\n", $2, $1}'
 
+######################################################
+echo
+echo Top 5 response status codes:
+grep -E -o 'HTTP/1.1"[ ]*([0-9]{3})[ ]*' $LOG | awk '{print $2}' \
+	| sort | uniq -c | sort -nr | head -5 \
+	| awk '{printf "%d - %d requests\n", $2, $1}'
 
 
 
